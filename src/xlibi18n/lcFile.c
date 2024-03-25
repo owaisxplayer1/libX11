owaisxplayer1/libX11

commit 225a4bbbbd174e26cc408640e10f3c37f81f3a3f

unifdef __UNIXOS2__
Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

src/xlibi18n/lcFile.c
@@ -46,11 +46,7 @@
 #define isreadable(f)	((access((f), R_OK) != -1) ? 1 : 0)
 #endif
 
-#ifndef __UNIXOS2__
 #define LC_PATHDELIM ':'
-#else
-#define LC_PATHDELIM ';'
-#endif
 
 #define XLC_BUFSIZE 256
 
@@ -92,75 +88,6 @@ parse_line(
     return argc;
 }
 
-#ifdef __UNIXOS2__
-
-/* fg021216: entries in locale files are separated by colons while under
-   OS/2, path entries are separated by semicolon, so we need two functions */
-
-static int
-parse_line1(
-    char *line,
-    char **argv,
-    int argsize)
-{
-    int argc = 0;
-    char *p = line;
-
-    while (argc < argsize) {
-	while (isspace(*p)) {
-	    ++p;
-	}
-	if (*p == '\0') {
-	    break;
-	}
-	argv[argc++] = p;
-	while (*p != ';' && *p != '\n' && *p != '\0') {
-	    ++p;
-	}
-	if (*p == '\0') {
-	    break;
-	}
-	*p++ = '\0';
-    }
-
-    return argc;
-}
-#elif defined(WIN32)
-
-/* this is parse_line but skips drive letters at the beginning of the entry */
-static int
-parse_line1(
-    char *line,
-    char **argv,
-    int argsize)
-{
-    int argc = 0;
-    char *p = line;
-
-    while (argc < argsize) {
-	while (isspace(*p)) {
-	    ++p;
-	}
-	if (*p == '\0') {
-	    break;
-	}
-	argv[argc++] = p;
-        if (isalpha(*p) && p[1] == ':') {
-            p+= 2; /* skip drive letters */
-        }
-	while (*p != ':' && *p != '\n' && *p != '\0') {
-	    ++p;
-	}
-	if (*p == '\0') {
-	    break;
-	}
-	*p++ = '\0';
-    }
-
-    return argc;
-}
-
-#endif   /* __UNIXOS2__ */
 
 /* Splits a colon separated list of directories, and returns the constituent
    paths (without trailing slash). At most argsize constituents are stored
@@ -174,7 +101,7 @@ _XlcParsePath(
     char *p = path;
     int n, i;
 
-#if !defined(__UNIXOS2__) && !defined(WIN32)
+#ifndef WIN32
     n = parse_line(path, argv, argsize);
 #else
     n = parse_line1(path, argv, argsize);
@@ -265,11 +192,7 @@ xlocaledir(
 #endif /* NO_XLOCALEDIR */
 
     if (len < buf_len)
-#ifndef __UNIXOS2__
       strncpy(p, XLOCALEDIR, (size_t) (buf_len - len));
-#else
-      strncpy(p,__XOS2RedirRoot(XLOCALEDIR), buf_len - len);
-#endif
     buf[buf_len-1] = '\0';
 }
 
@@ -343,11 +266,7 @@ xlocalelibdir(
 #endif /* NO_XLOCALEDIR */
 
     if (len < buf_len)
-#ifndef __UNIXOS2__
       strncpy(p, XLOCALELIBDIR, (size_t) (buf_len - len));
-#else
-      strncpy(p,__XOS2RedirRoot(XLOCALELIBDIR), (size_t) (buf_len - len));
-#endif
     buf[buf_len-1] = '\0';
 }
 
@@ -374,17 +293,6 @@ resolve_name(
 	char *p = buf;
 	int n;
 	char *args[2], *from, *to;
-#ifdef __UNIXOS2__  /* Take out CR under OS/2 */
-	int len;
-
-	len = strlen(p);
-	if (len > 1) {
-	    if (*(p+len-2) == '\r' && *(p+len-1) == '\n') {
-		*(p+len-2) = '\n';
-		*(p+len-1) = '\0';
-	    }
-	}
-#endif
 	while (isspace(*p)) {
 	    ++p;
 	}

include/X11/Xlib.h
@@ -50,12 +50,8 @@ in this Software without prior written authorization from The Open Group.
 #ifndef X_WCHAR
 #include <stddef.h>
 #else
-#ifdef __UNIXOS2__
-#include <stdlib.h>
-#else
 /* replace this with #include or typedef appropriate for your system */
 typedef unsigned long wchar_t;
-#endif
 #endif
 
 

include/X11/Xlibint.h
@@ -1327,12 +1327,6 @@ struct _XConnWatchInfo {	/* info from XAddConnectionWatch */
     struct _XConnWatchInfo *next;
 };
 
-#ifdef __UNIXOS2__
-extern char* __XOS2RedirRoot(
-    char*
-);
-#endif
-
 extern int _XTextHeight(
     XFontStruct*	/* font_struct */,
     _Xconst char*	/* string */,

src/CrGlCur.c
@@ -31,10 +31,6 @@ in this Software without prior written authorization from The Open Group.
 
 #ifdef USE_DYNAMIC_XCURSOR
 
-#ifdef __UNIXOS2__
-#define RTLD_LAZY 1
-#define LIBXCURSOR "Xcursor.dll"
-#endif
 #include <stdio.h>
 #include <string.h>
 #if defined(hpux)

src/GetDflt.c
@@ -184,10 +184,6 @@ XGetDefault(
 #ifdef WIN32
 	char *progname2;
 #endif
-#ifdef __UNIXOS2__
-	char *progname2;
-	char *dotpos;
-#endif
 
 	/*
 	 * strip path off of program name (XXX - this is OS specific)
@@ -198,13 +194,6 @@ XGetDefault(
 	if (progname2 && (!progname || progname < progname2))
 	    progname = progname2;
 #endif
-#ifdef __UNIXOS2__  /* Very similar to WIN32 */
-	progname2 = strrchr (prog, '\\');
-	if (progname2 && (!progname || progname < progname2))
-	    progname = progname2;
-	dotpos = strrchr (prog, '.');
-	if (dotpos && (dotpos>progname2)) *dotpos='\0';
-#endif  /* We take out the .exe suffix  */
 
 	if (progname)
 	    progname++;

src/RdBitF.c
@@ -129,9 +129,6 @@ XReadBitmapFileData (
     int hx = -1;			/* x hotspot */
     int hy = -1;			/* y hotspot */
 
-#ifdef __UNIXOS2__
-    filename = __XOS2RedirRoot(filename);
-#endif
     if (!(fstream = fopen(filename, "r")))
 	return BitmapOpenFailed;
 

src/SetLocale.c
@@ -99,7 +99,7 @@ _XlcMapOSLocaleName(
     char *osname,
     char *siname)
 {
-#if defined(hpux) || defined(CSRG_BASED) || defined(sun) || defined(SVR4) || defined(sgi) || defined(WIN32) || defined(__UNIXOS2__) || defined(linux)
+#if defined(hpux) || defined(CSRG_BASED) || defined(sun) || defined(SVR4) || defined(sgi) || defined(WIN32) || defined(linux)
 # ifdef hpux
 #  ifndef _LastCategory
    /* HPUX 9 and earlier */
@@ -111,7 +111,7 @@ _XlcMapOSLocaleName(
 #   define ENDCHAR ' '
 #  endif
 # else
-#  if defined(WIN32) || defined(__UNIXOS2__)
+#  if defined(WIN32)
 #   define SKIPCOUNT 1
 #   define STARTCHAR '='
 #   define ENDCHAR ';'

src/WMProps.c
@@ -122,14 +122,6 @@ void XSetWMProperties (
 		 * systems will have to change this.
 		 */
 		char *cp = strrchr (argv[0], '/');
-#ifdef __UNIXOS2__
-		char *os2_cp = strrchr (argv[0],'\\');
-		char *dot_cp = strrchr (argv[0],'.');
-		if (os2_cp && (os2_cp > cp)) {
-		    if(dot_cp && (dot_cp > os2_cp)) *dot_cp = '\0';
-		    cp=os2_cp;
-		}
-#endif
 		tmp.res_name = (cp ? cp + 1 : argv[0]);
 	    }
 	    tmp.res_class = classHints->res_class;

commit 9afd55ada5d7a3c1d3ed23ff8451629a1d989a7f

xlibi18n: restore parse_line1 for WIN32 builds
Accidentally removed by __UNIXOS2__ cleanup
Closes: #204
Fixes: 225a4bbb ("unifdef __UNIXOS2__")

Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>

src/xlibi18n/lcFile.c
@@ -88,6 +88,40 @@ parse_line(
     return argc;
 }
 
+#ifdef WIN32
+/* this is parse_line but skips drive letters at the beginning of the entry */
+static int
+parse_line1(
+    char *line,
+    char **argv,
+    int argsize)
+{
+    int argc = 0;
+    char *p = line;
+
+    while (argc < argsize) {
+	while (isspace(*p)) {
+	    ++p;
+	}
+	if (*p == '\0') {
+	    break;
+	}
+	argv[argc++] = p;
+        if (isalpha(*p) && p[1] == ':') {
+            p+= 2; /* skip drive letters */
+        }
+	while (*p != ':' && *p != '\n' && *p != '\0') {
+	    ++p;
+	}
+	if (*p == '\0') {
+	    break;
+	}
+	*p++ = '\0';
+    }
+
+    return argc;
+}
+#endif   /* WIN32 */
 
 /* Splits a colon separated list of directories, and returns the constituent
    paths (without trailing slash). At most argsize constituents are stored