owaisxplayer1/libX11

commit 024d229fdf88a7755577b01b46af6ef908d599e0

ximcp: Unmark to fabricate key events with XKeyEvent serial
_XimProtoKeypressFilter() and _XimProtoKeyreleaseFilter() can
receive XKeyEvent from both the typing on the keyboard and the
callback of XIM_FORWARD_EVENT.

If the filter functions unmark to fabricate XKeyEvent from the typing
on the keyboard during receiving XKeyEvent from the callback of
XIM_FORWARD_EVENT with typing keys very quickly likes an bar code
scanner (or evemu-play), XIM server cannot receive some key events and
it causes the key typing order to get scrambled.

Now XIM client saves the serial in XKeyEvent and the filter functions
unmark to fabricate XKeyEvent from the callback of XIM_FORWARD_EVENT
only.

Fixes: #198

modules/im/ximcp/imDefFlt.c
@@ -142,9 +142,9 @@ _XimProtoKeypressFilter(
 {
     Xim		im = (Xim)ic->core.im;
 
-    if (IS_FABRICATED(im)) {
+    if (_XimIsFabricatedSerial(im, ev->serial)) {
 	_XimPendingFilter(ic);
-	UNMARK_FABRICATED(im);
+	_XimUnfabricateSerial(im, ev->serial);
 	return NOTFILTERD;
     }
 
@@ -203,9 +203,9 @@ _XimProtoKeyreleaseFilter(
 {
     Xim		im = (Xim)ic->core.im;
 
-    if (IS_FABRICATED(im)) {
+    if (_XimIsFabricatedSerial(im, ev->serial)) {
 	_XimPendingFilter(ic);
-	UNMARK_FABRICATED(im);
+	_XimUnfabricateSerial(im, ev->serial);
 	return NOTFILTERD;
     }
 

modules/im/ximcp/imDefIm.c
@@ -430,6 +430,7 @@ _XimPreConnect(
 	return False;
 
     im->private.proto.im_window = im_window;
+    im->private.proto.fabricated_serial = 0;
     return True;
 }
 

modules/im/ximcp/imDefLkup.c
@@ -341,6 +341,54 @@ _XimForwardEvent(
     return _XimForwardEventCore(ic, ev, sync);
 }
 
+Bool
+_XimFabricateSerial(
+    Xim			 im,
+    unsigned long	 serial)
+{
+    if (!serial)
+	return False;
+    if (serial == im->private.proto.fabricated_serial) {
+	fprintf(stderr, "%s,%d: The key event is already fabricated.\n", __FILE__, __LINE__);
+	return False;
+    }
+    if (im->private.proto.fabricated_serial)
+	fprintf(stderr, "%s,%d: Tried to fabricate a wrong key event.\n", __FILE__, __LINE__);
+
+    MARK_FABRICATED(im);
+    im->private.proto.fabricated_serial = serial;
+    return True;
+}
+
+Bool
+_XimUnfabricateSerial(
+    Xim			 im,
+    unsigned long	 serial)
+{
+    if (!serial)
+	return False;
+    if (!im->private.proto.fabricated_serial) {
+	fprintf(stderr, "%s,%d: The key event is already unfabricated.\n", __FILE__, __LINE__);
+	return False;
+    }
+    if (serial != im->private.proto.fabricated_serial)
+	fprintf(stderr, "%s,%d: Tried to unfabricate a wrong key event.\n", __FILE__, __LINE__);
+
+    im->private.proto.fabricated_serial = 0;
+    UNMARK_FABRICATED(im);
+    return True;
+}
+
+Bool
+_XimIsFabricatedSerial(
+    Xim			 im,
+    unsigned long	 serial)
+{
+    if (!serial)
+	return False;
+    return (serial == im->private.proto.fabricated_serial);
+}
+
 static void
 _XimProcEvent(
     Display		*d,
@@ -355,7 +403,7 @@ _XimProcEvent(
     ev->xany.serial |= serial << 16;
     ev->xany.send_event = False;
     ev->xany.display = d;
-    MARK_FABRICATED(ic->core.im);
+    _XimFabricateSerial((Xim)ic->core.im, ev->xany.serial);
     return;
 }
 
@@ -704,10 +752,6 @@ _XimCommitRecv(
 
     (void)_XimRespSyncReply(ic, flag);
 
-    if (ic->private.proto.registed_filter_event
-	& (KEYPRESS_MASK | KEYRELEASE_MASK))
-	    MARK_FABRICATED(im);
-
     bzero(&ev, sizeof(ev));	/* uninitialized : found when running kterm under valgrind */
 
     ev.type = KeyPress;
@@ -719,6 +763,10 @@ _XimCommitRecv(
 
     ev.time = 0L;
     ev.serial = LastKnownRequestProcessed(im->core.display);
+
+    if (ic->private.proto.registed_filter_event
+	& (KEYPRESS_MASK | KEYRELEASE_MASK))
+	    _XimFabricateSerial(im, ev.serial);
     /* FIXME :
        I wish there were COMMENTs (!) about the data passed around.
     */

src/xlibi18n/XimintP.h
@@ -149,6 +149,8 @@ typedef struct _XimProtoPrivateRec {
     XimTransRegDispatcher	 register_dispatcher;
     XimTransCallDispatcher	 call_dispatcher;
     XPointer			 spec;
+
+    unsigned long                fabricated_serial;
 } XimProtoPrivateRec;
 
 /*
@@ -306,5 +308,20 @@ typedef struct _XicProtoPrivateRec {
 
 #define XIM_MAXIMNAMELEN 64
 #define XIM_MAXLCNAMELEN 64
+
+Bool
+_XimFabricateSerial(
+    Xim                  im,
+    unsigned long        serial);
+
+Bool
+_XimUnfabricateSerial(
+    Xim                  im,
+    unsigned long        serial);
+
+Bool
+_XimIsFabricatedSerial(
+    Xim                  im,
+    unsigned long        serial);
 
 #endif /* _XIMINTP_H */

commit 5a14178c7cc408f425fe298aeade3dee749b1ca1

ximcp: Add fabricated_time in XimProtoPrivate for timeout
When users type keys quickly, some applications using Steam or Java
do not call XNextEvent() for a key event but _XimFilterKeypress()
and _XimFilterKeyrelease() expect to receive the key events
forwarded by input methods.

Now fabricated_time Time value is added to XimProtoPrivate to check
the timeout value.

Closes: #205
Fixes: 024d229f ("ximcp: Unmark to fabricate key events with XKeyEvent serial")
Part-of: <https://gitlab.freedesktop.org/xorg/lib/libx11/-/merge_requests/246>

modules/im/ximcp/imDefFlt.c
@@ -142,9 +142,9 @@ _XimProtoKeypressFilter(
 {
     Xim		im = (Xim)ic->core.im;
 
-    if (_XimIsFabricatedSerial(im, ev->serial)) {
+    if (_XimIsFabricatedSerial(im, ev)) {
 	_XimPendingFilter(ic);
-	_XimUnfabricateSerial(im, ev->serial);
+	_XimUnfabricateSerial(im, ev);
 	return NOTFILTERD;
     }
 
@@ -203,9 +203,9 @@ _XimProtoKeyreleaseFilter(
 {
     Xim		im = (Xim)ic->core.im;
 
-    if (_XimIsFabricatedSerial(im, ev->serial)) {
+    if (_XimIsFabricatedSerial(im, ev)) {
 	_XimPendingFilter(ic);
-	_XimUnfabricateSerial(im, ev->serial);
+	_XimUnfabricateSerial(im, ev);
 	return NOTFILTERD;
     }
 

modules/im/ximcp/imDefIm.c
@@ -431,6 +431,8 @@ _XimPreConnect(
 
     im->private.proto.im_window = im_window;
     im->private.proto.fabricated_serial = 0;
+    im->private.proto.fabricated_time = 0;
+    im->private.proto.enable_fabricated_order = True;
     return True;
 }
 

modules/im/ximcp/imDefLkup.c
@@ -344,14 +344,14 @@ _XimForwardEvent(
 Bool
 _XimFabricateSerial(
     Xim			 im,
-    unsigned long	 serial)
+    XKeyEvent		*event)
 {
     /* GTK2 XIM module sets serial=0. */
-    if (!serial) {
+    if (!event->serial || !im->private.proto.enable_fabricated_order) {
 	MARK_FABRICATED(im);
 	return True;
     }
-    if (serial == im->private.proto.fabricated_serial) {
+    if (event->serial == im->private.proto.fabricated_serial) {
 	fprintf(stderr, "%s,%d: The key event is already fabricated.\n", __FILE__, __LINE__);
 	return False;
     }
@@ -359,17 +359,18 @@ _XimFabricateSerial(
 	fprintf(stderr, "%s,%d: Tried to fabricate a wrong key event.\n", __FILE__, __LINE__);
 
     MARK_FABRICATED(im);
-    im->private.proto.fabricated_serial = serial;
+    im->private.proto.fabricated_serial = event->serial;
+    im->private.proto.fabricated_time = event->time;
     return True;
 }
 
 Bool
 _XimUnfabricateSerial(
     Xim			 im,
-    unsigned long	 serial)
+    XKeyEvent		*event)
 {
     /* GTK2 XIM module sets serial=0. */
-    if (!serial) {
+    if (!event->serial || !im->private.proto.enable_fabricated_order) {
 	UNMARK_FABRICATED(im);
 	return True;
     }
@@ -377,10 +378,11 @@ _XimUnfabricateSerial(
 	fprintf(stderr, "%s,%d: The key event is already unfabricated.\n", __FILE__, __LINE__);
 	return False;
     }
-    if (serial != im->private.proto.fabricated_serial)
+    if (event->serial != im->private.proto.fabricated_serial)
 	fprintf(stderr, "%s,%d: Tried to unfabricate a wrong key event.\n", __FILE__, __LINE__);
 
     im->private.proto.fabricated_serial = 0;
+    im->private.proto.fabricated_time = 0;
     UNMARK_FABRICATED(im);
     return True;
 }
@@ -388,12 +390,32 @@ _XimUnfabricateSerial(
 Bool
 _XimIsFabricatedSerial(
     Xim			 im,
-    unsigned long	 serial)
+    XKeyEvent		*event)
 {
     /* GTK2 XIM module sets serial=0. */
-    if (!serial)
-	return IS_FABRICATED(im);
-    return (serial == im->private.proto.fabricated_serial);
+    if (!event->serial || !im->private.proto.enable_fabricated_order)
+	return IS_FABRICATED(im) ? True : False;
+    if (event->serial == im->private.proto.fabricated_serial)
+	return True;
+    if (!im->private.proto.fabricated_serial)
+	return False;
+    /* Rotate time */
+    if (event->time < im->private.proto.fabricated_time) {
+	if (event->time >= 1000)
+	    im->private.proto.fabricated_time = 0;
+    } else if (event->time - im->private.proto.fabricated_time > 1000) {
+	fprintf(stderr,
+	        "%s,%d: The application disposed a key event with %ld serial.\n",
+	        __FILE__, __LINE__,
+	        im->private.proto.fabricated_serial);
+	im->private.proto.enable_fabricated_order = False;
+	if (IS_FABRICATED(im)) {
+	    if (event->serial)
+		im->private.proto.fabricated_serial = event->serial;
+	    return True;
+	}
+    }
+    return False;
 }
 
 static void
@@ -410,7 +432,7 @@ _XimProcEvent(
     ev->xany.serial |= serial << 16;
     ev->xany.send_event = False;
     ev->xany.display = d;
-    _XimFabricateSerial((Xim)ic->core.im, ev->xany.serial);
+    _XimFabricateSerial((Xim)ic->core.im, &ev->xkey);
     return;
 }
 
@@ -811,7 +833,7 @@ _XimCommitRecv(
 
     if (ic->private.proto.registed_filter_event
 	& (KEYPRESS_MASK | KEYRELEASE_MASK))
-	    _XimFabricateSerial(im, ev.serial);
+	    _XimFabricateSerial(im, &ev);
     /* FIXME :
        I wish there were COMMENTs (!) about the data passed around.
     */

src/xlibi18n/XimintP.h
@@ -150,7 +150,9 @@ typedef struct _XimProtoPrivateRec {
     XimTransCallDispatcher	 call_dispatcher;
     XPointer			 spec;
 
-    unsigned long                fabricated_serial;
+    unsigned long		 fabricated_serial;
+    Time			 fabricated_time;
+    Bool			 enable_fabricated_order;
 } XimProtoPrivateRec;
 
 /*
@@ -312,16 +314,16 @@ typedef struct _XicProtoPrivateRec {
 Bool
 _XimFabricateSerial(
     Xim                  im,
-    unsigned long        serial);
+    XKeyEvent           *event);
 
 Bool
 _XimUnfabricateSerial(
     Xim                  im,
-    unsigned long        serial);
+    XKeyEvent           *event);
 
 Bool
 _XimIsFabricatedSerial(
     Xim                  im,
-    unsigned long        serial);
+    XKeyEvent           *event);
 
 #endif /* _XIMINTP_H */