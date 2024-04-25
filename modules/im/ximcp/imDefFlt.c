owaisxplayer1/libX11

commit 024d229fdf88a7755577b01b46af6ef908d599e0

ximcp: Unmark to fabricate key events with XKeyEvent serial
_XimProtoKeypressFilter() and _XimProtoKeyreleaseFilter() can
receive XKeyEvent from both the typing on the keyboard and the
callback of XIM_FORWARD_EVENT.

If the filter functions unmark to fabricate XKeyEvent from the typing
on the keyboard during receiving XKeyEvent from the callback of
XIM_FORWARD_EVENT with typing keys very quickly likes an bar code
scanner (or evemu-play), XIM server cannot receive some key events and
it causes the key typing order to get scrambled.

Now XIM client saves the serial in XKeyEvent and the filter functions
unmark to fabricate XKeyEvent from the callback of XIM_FORWARD_EVENT
only.

Fixes: #198

modules/im/ximcp/imDefFlt.c
@@ -142,9 +142,9 @@ _XimProtoKeypressFilter(
 {
     Xim		im = (Xim)ic->core.im;
 
-    if (IS_FABRICATED(im)) {
+    if (_XimIsFabricatedSerial(im, ev->serial)) {
 	_XimPendingFilter(ic);
-	UNMARK_FABRICATED(im);
+	_XimUnfabricateSerial(im, ev->serial);
 	return NOTFILTERD;
     }
 
@@ -203,9 +203,9 @@ _XimProtoKeyreleaseFilter(
 {
     Xim		im = (Xim)ic->core.im;
 
-    if (IS_FABRICATED(im)) {
+    if (_XimIsFabricatedSerial(im, ev->serial)) {
 	_XimPendingFilter(ic);
-	UNMARK_FABRICATED(im);
+	_XimUnfabricateSerial(im, ev->serial);
 	return NOTFILTERD;
     }
 

modules/im/ximcp/imDefIm.c
@@ -430,6 +430,7 @@ _XimPreConnect(
 	return False;
 
     im->private.proto.im_window = im_window;
+    im->private.proto.fabricated_serial = 0;
     return True;
 }
 

modules/im/ximcp/imDefLkup.c
@@ -341,6 +341,54 @@ _XimForwardEvent(
     return _XimForwardEventCore(ic, ev, sync);
 }
 
+Bool
+_XimFabricateSerial(
+    Xim			 im,
+    unsigned long	 serial)
+{
+    if (!serial)
+	return False;
+    if (serial == im->private.proto.fabricated_serial) {
+	fprintf(stderr, "%s,%d: The key event is already fabricated.\n", __FILE__, __LINE__);
+	return False;
+    }
+    if (im->private.proto.fabricated_serial)
+	fprintf(stderr, "%s,%d: Tried to fabricate a wrong key event.\n", __FILE__, __LINE__);
+
+    MARK_FABRICATED(im);
+    im->private.proto.fabricated_serial = serial;
+    return True;
+}
+
+Bool
+_XimUnfabricateSerial(
+    Xim			 im,
+    unsigned long	 serial)
+{
+    if (!serial)
+	return False;
+    if (!im->private.proto.fabricated_serial) {
+	fprintf(stderr, "%s,%d: The key event is already unfabricated.\n", __FILE__, __LINE__);
+	return False;
+    }
+    if (serial != im->private.proto.fabricated_serial)
+	fprintf(stderr, "%s,%d: Tried to unfabricate a wrong key event.\n", __FILE__, __LINE__);
+
+    im->private.proto.fabricated_serial = 0;
+    UNMARK_FABRICATED(im);
+    return True;
+}
+
+Bool
+_XimIsFabricatedSerial(
+    Xim			 im,
+    unsigned long	 serial)
+{
+    if (!serial)
+	return False;
+    return (serial == im->private.proto.fabricated_serial);
+}
+
 static void
 _XimProcEvent(
     Display		*d,
@@ -355,7 +403,7 @@ _XimProcEvent(
     ev->xany.serial |= serial << 16;
     ev->xany.send_event = False;
     ev->xany.display = d;
-    MARK_FABRICATED(ic->core.im);
+    _XimFabricateSerial((Xim)ic->core.im, ev->xany.serial);
     return;
 }
 
@@ -704,10 +752,6 @@ _XimCommitRecv(
 
     (void)_XimRespSyncReply(ic, flag);
 
-    if (ic->private.proto.registed_filter_event
-	& (KEYPRESS_MASK | KEYRELEASE_MASK))
-	    MARK_FABRICATED(im);
-
     bzero(&ev, sizeof(ev));	/* uninitialized : found when running kterm under valgrind */
 
     ev.type = KeyPress;
@@ -719,6 +763,10 @@ _XimCommitRecv(
 
     ev.time = 0L;
     ev.serial = LastKnownRequestProcessed(im->core.display);
+
+    if (ic->private.proto.registed_filter_event
+	& (KEYPRESS_MASK | KEYRELEASE_MASK))
+	    _XimFabricateSerial(im, ev.serial);
     /* FIXME :
        I wish there were COMMENTs (!) about the data passed around.
     */

src/xlibi18n/XimintP.h
@@ -149,6 +149,8 @@ typedef struct _XimProtoPrivateRec {
     XimTransRegDispatcher	 register_dispatcher;
     XimTransCallDispatcher	 call_dispatcher;
     XPointer			 spec;
+
+    unsigned long                fabricated_serial;
 } XimProtoPrivateRec;
 
 /*
@@ -306,5 +308,20 @@ typedef struct _XicProtoPrivateRec {
 
 #define XIM_MAXIMNAMELEN 64
 #define XIM_MAXLCNAMELEN 64
+
+Bool
+_XimFabricateSerial(
+    Xim                  im,
+    unsigned long        serial);
+
+Bool
+_XimUnfabricateSerial(
+    Xim                  im,
+    unsigned long        serial);
+
+Bool
+_XimIsFabricatedSerial(
+    Xim                  im,
+    unsigned long        serial);
 
 #endif /* _XIMINTP_H */

commit 5a1e62d77b65ba148b1c6d1d22a81dc2b07e7d9e

Accept anon windows in XFilterEvent to update XIM state
When input focuses are switched quickly with shortcut keys in a Java
window, the focus is sometimes lost and the Window=0 is assigned in
XFilterEvent() but the XKeyEvent was forwarded by a XIM serer(IBus)
with XIM_FORWARD_EVENT -> XNextEvent() -> XFilterEvent() and the event
needs to be forwarded to the XIM XKeyEvent press and release filters
to update the XIM state with Window=0 likes _XimPendingFilter() and
_XimUnfabricateSerial().

Closes: #205, #206
Fixes: 024d229f ("ximcp: Unmark to fabricate key events with XKeyEvent serial")
Part-of: <https://gitlab.freedesktop.org/xorg/lib/libx11/-/merge_requests/246>

modules/im/ximcp/imDefFlt.c
@@ -138,7 +138,8 @@ _XimPendingFilter(
 static Bool
 _XimProtoKeypressFilter(
     Xic		 ic,
-    XKeyEvent	*ev)
+    XKeyEvent	*ev,
+    Window	 w)
 {
     Xim		im = (Xim)ic->core.im;
 
@@ -147,6 +148,9 @@ _XimProtoKeypressFilter(
 	_XimUnfabricateSerial(im, ev);
 	return NOTFILTERD;
     }
+    /* w=0 is used for _XimIsFabricatedSerial() only */
+    if (!w)
+	return NOTFILTERD;
 
     if (IS_NEGLECT_EVENT(ic, KeyPressMask))
 	return FILTERD;
@@ -193,13 +197,14 @@ _XimFilterKeypress(
     XEvent		*ev,
     XPointer		 client_data)
 {
-    return _XimProtoKeypressFilter((Xic)client_data, (XKeyEvent *)ev );
+    return _XimProtoKeypressFilter((Xic)client_data, (XKeyEvent *)ev, w);
 }
 
 static Bool
 _XimProtoKeyreleaseFilter(
     Xic		 ic,
-    XKeyEvent	*ev)
+    XKeyEvent	*ev,
+    Window	 w)
 {
     Xim		im = (Xim)ic->core.im;
 
@@ -208,6 +213,9 @@ _XimProtoKeyreleaseFilter(
 	_XimUnfabricateSerial(im, ev);
 	return NOTFILTERD;
     }
+    /* w=0 is used for _XimIsFabricatedSerial() only */
+    if (!w)
+	return NOTFILTERD;
 
     if (IS_NEGLECT_EVENT(ic, KeyReleaseMask))
 	return FILTERD;
@@ -254,7 +262,7 @@ _XimFilterKeyrelease(
     XEvent		*ev,
     XPointer		 client_data)
 {
-    return _XimProtoKeyreleaseFilter((Xic)client_data, (XKeyEvent *)ev);
+    return _XimProtoKeyreleaseFilter((Xic)client_data, (XKeyEvent *)ev, w);
 }
 
 static void
@@ -263,6 +271,11 @@ _XimRegisterKeyPressFilter(
 {
     if (ic->core.focus_window) {
 	if (!(ic->private.proto.registed_filter_event & KEYPRESS_MASK)) {
+	    _XRegisterFilterByType (ic->core.im->core.display,
+				    0,
+				    KeyPress, KeyPress,
+				    _XimFilterKeypress,
+				    (XPointer)ic);
 	    _XRegisterFilterByType (ic->core.im->core.display,
 				    ic->core.focus_window,
 				    KeyPress, KeyPress,
@@ -280,6 +293,11 @@ _XimRegisterKeyReleaseFilter(
 {
     if (ic->core.focus_window) {
 	if (!(ic->private.proto.registed_filter_event & KEYRELEASE_MASK)) {
+	    _XRegisterFilterByType (ic->core.im->core.display,
+				    0,
+				    KeyRelease, KeyRelease,
+				    _XimFilterKeyrelease,
+				    (XPointer)ic);
 	    _XRegisterFilterByType (ic->core.im->core.display,
 				    ic->core.focus_window,
 				    KeyRelease, KeyRelease,
@@ -301,6 +319,10 @@ _XimUnregisterKeyPressFilter(
 				ic->core.focus_window,
 				_XimFilterKeypress,
 				(XPointer)ic);
+	    _XUnregisterFilter (ic->core.im->core.display,
+				0,
+				_XimFilterKeypress,
+				(XPointer)ic);
 	    ic->private.proto.registed_filter_event &= ~KEYPRESS_MASK;
 	}
     }
@@ -315,6 +337,10 @@ _XimUnregisterKeyReleaseFilter(
 	if (ic->private.proto.registed_filter_event & KEYRELEASE_MASK) {
 	    _XUnregisterFilter (ic->core.im->core.display,
 				ic->core.focus_window,
+				_XimFilterKeyrelease,
+				(XPointer)ic);
+	    _XUnregisterFilter (ic->core.im->core.display,
+				0,
 				_XimFilterKeyrelease,
 				(XPointer)ic);
 	    ic->private.proto.registed_filter_event &= ~KEYRELEASE_MASK;

src/FilterEv.c
@@ -100,6 +100,31 @@ XFilterEvent(
 	    }
 	}
     }
+    for (p = ev->xany.display->im_filters; p != NULL; p = p->next) {
+	/* Java sometimes calls XFilterEvent() with window=0 and ev come from
+	 * XNextEvent() when users type some keys quickly and switch multiple
+	 * input focuses in a Java window with the keys.
+	 * But XKeyEvent filters need to receive the event with window=0 for
+	 * _XimPendingFilter() and _XimUnfabricateSerial() to clear the
+	 * fowarded XKeyEvent with XIM_FORWARD_EVENT.
+	 *
+	 * The case of p->window == 0 is checkekd after all cases of p->window
+	 * != 0 are checked because all input contexts share
+	 * Display->im_filters but each input context has
+	 * Xic->private.proto.registed_filter_event for the filters
+	 * and same p->filter could be registerd to Display->im_filters twice
+	 * with different p->window.
+	 */
+	if (p->window == 0 && window == 0) {
+	    if ((mask & p->event_mask) ||
+		(ev->type >= p->start_type && ev->type <= p->end_type)) {
+		UnlockDisplay(ev->xany.display);
+		ret = (*(p->filter))(ev->xany.display, p->window, ev,
+				      p->client_data);
+		return(ret);
+	    }
+	}
+    }
     UnlockDisplay(ev->xany.display);
 #endif
     return(False);