owaisxplayer1/libX11

commit 8ba0ca32a63c532f128bdca7f1bf982cab8e12be

Janitor: ansification, make distcheck, compiler warnings.
  Only convert to use "ansi prototypes" the functions warned from
compilation with "./autogen.sh --prefix=/usr", on a Linux computer.

  Also, only address "trivial" compiler warning fixes in this commit.

  The new .gitignore is the output of a command like:
% find . -name .gitignore -exec cat {} \; | sort | uniq
and only the toplevel .gitignore file was kept.

src/xkb/XKBMisc.c
@@ -669,7 +669,7 @@ register int	i;
 int		width,nOldGroups,oldWidth,newTypes[XkbNumKbdGroups];
 
     if ((!xkb) || (!XkbKeycodeInRange(xkb,key)) || (!xkb->map) ||
-	(!xkb->map->types)||(!newTypes)||((groups&XkbAllGroupsMask)==0)||
+	(!xkb->map->types)||((groups&XkbAllGroupsMask)==0)||
 	(nGroups>XkbNumKbdGroups)) {
 	return BadMatch;
     }

commit 88d1a84ca3c0f87ffcb83b9b96e34a7121d5e2d3

XkbChangeTypesOfKey: verify newTypesIn is not null before using it
Based on xorg/xserver@97c9e6a7139e07a0761be05034fae42fcd65ba36

Fixes: 8ba0ca32 ("Janitor: ansification, make distcheck, compiler warnings.")
Signed-off-by: Alan Coopersmith <alan.coopersmith@oracle.com>
Part-of: <https://gitlab.freedesktop.org/xorg/lib/libx11/-/merge_requests/279>

src/xkb/XKBMisc.c
@@ -682,8 +682,8 @@ XkbChangeTypesOfKey(XkbDescPtr xkb,
     int width, nOldGroups, oldWidth, newTypes[XkbNumKbdGroups];
 
     if ((!xkb) || (!XkbKeycodeInRange(xkb, key)) || (!xkb->map) ||
-        (!xkb->map->types) || ((groups & XkbAllGroupsMask) == 0) ||
-        (nGroups > XkbNumKbdGroups)) {
+        (!xkb->map->types) || (!newTypesIn) ||
+        ((groups & XkbAllGroupsMask) == 0) || (nGroups > XkbNumKbdGroups)) {
         return BadMatch;
     }
     if (nGroups == 0) {